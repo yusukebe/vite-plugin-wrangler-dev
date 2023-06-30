ca941f2977b2d102522c82a5f896d597e3a7a1b7
Yusuke Wada <yusuke@cloudflare.com>
2023-07-01 01:53:51 +0900

    use wrangler

--- server.ts
+++ server.ts
@@ -1,27 +1,67 @@
 import express from 'express'
-import { createServer as createViteServer } from 'vite'
-import type { Hono } from 'hono'
+import { createServer as createViteServer, build as viteBuild } from 'vite'
 import { getRequestListener } from '@hono/node-server'
+import path from 'path'
+import { unstable_dev } from 'wrangler'
+import type { UnstableDevWorker } from 'wrangler'
 
 async function createServer() {
-  const server = express()
+  let worker: UnstableDevWorker
+
+  const rebuildAndRestart = async () => {
+    await buildScript('./src/app.ts')
+    worker = await unstable_dev('dist/app.mjs', {
+      experimental: { disableExperimentalWarning: true },
+      logLevel: 'info'
+    })
+  }
+
+  const watchAndBuildPlugin = {
+    name: 'watch-and-build',
+    async handleHotUpdate({ file }: { file: string }) {
+      if (file.startsWith(path.resolve(__dirname, './src/'))) {
+        await rebuildAndRestart()
+      }
+    }
+  }
 
   const vite = await createViteServer({
     server: { middlewareMode: true },
-    appType: 'custom'
+    appType: 'custom',
+    plugins: [watchAndBuildPlugin]
   })
 
+  // Initial build
+  await rebuildAndRestart()
+
+  // Express as dev server
+  const server = express()
   server.use(vite.middlewares)
 
   server.use('*', async (req, res) => {
-    const { app } = (await vite.ssrLoadModule('/src/app.ts')) as { app: Hono }
-
+    await vite.ssrLoadModule('/src/app.ts')
     req.url = req.originalUrl
-    getRequestListener(app.fetch)(req, res)
+    getRequestListener(async (workerRequest) => {
+      const newResponse = await worker.fetch(workerRequest.url, workerRequest as any)
+      return newResponse
+    })(req, res)
   })
 
   server.listen(5173, () => {
     console.log('http://localhost:5173')
+  })
+}
+
+async function buildScript(filename: string) {
+  await viteBuild({
+    ssr: {
+      noExternal: true,
+      format: 'esm'
+    },
+    build: {
+      ssr: path.resolve(__dirname, filename),
+      rollupOptions: {}
+    }
   })
 }
 
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import { Hono } from 'hono'
 import { html } from 'hono/html'
 
-const app = new Hono()
+type Bindings = {
+  TOKEN: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.html(
     html`<html>
       <body>
-        <h1>Hello Vite!!</h1>
+        <h1>Hello Wrangler!</h1>
+        <p>TOKEN is ${c.env.TOKEN}</p>
         <script type="module" src="/@vite/client"></script>
       </body>
     </html>`
@@ -16,4 +21,4 @@ app.get('/', (c) => {
 
 app.get('/api', (c) => c.json({ message: 'Hello Vite!!' }))
 
-export { app }
+export default app
